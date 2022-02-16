8fda8888f9b53d636e394015c3a583ea6af76a5c
Josiah <jwalutsachi4@gmail.com>
2022-02-16 14:40:34 +0300

    Added new tables to sql file: orders, products, categories. Modified user table to accomodate roles

--- database/create.sql
+++ database/create.sql
@@ -1,13 +1,46 @@
--- Create a new SQL Database
+-- Create new tables
 
-DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL AUTO_INCREMENT,
     fname VARCHAR(100) NOT NULL,
     lname VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL,
     password VARCHAR(250) NOT NULL,
+    role enum('ADMIN', 'CUSTOMER') NOT NULL,
     user_img BLOB,
-    PRIMARY KEY(id)
+    PRIMARY KEY(user_id)
 );
+
+
+
+CREATE TABLE categories (
+    category_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(category_id)
+);
+
+
+
+CREATE TABLE products (
+    product_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    category_id INT NOT NULL,
+    quantity INT,
+    unit_price DECIMAL(7, 2) NOT NULL,
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+
+CREATE TABLE orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    user_id INT NOT NULL,
+    quantity INT,
+    price DECIMAL(7, 2) NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
